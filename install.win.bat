23a881b8191d3fcecee8585df31c91208afa1de0
Lorne Guse <boomshankerx@hotmail.com>
2018-08-18 21:46:07 -0600

    Updated plugin list

--- install.win.bat
+++ install.win.bat
@@ -9,3 +9,5 @@ pause
 git clone https://github.com/gmarik/vundle.git %USERPROFILE%/vimfiles/bundle/vundle
 "C:\Program Files (x86)\Vim\vim80\gvim.exe" +PluginInstall +qall
 pause
+"%USERPROFILE\vimfiles\bundles\YouCompleteMe\install.py"
+pause
